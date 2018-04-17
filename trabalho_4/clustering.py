f4e1ffd0af42b48f843516a2c510e416c9479768
paulopieczarka <paulopieczarka@gmail.com>
2018-04-17 02:05:15 -0300

    Stating with Consulta SJR

--- trabalho_4/clustering.py
+++ trabalho_4/clustering.py
@@ -29,38 +29,60 @@ data.qualisScoreValue[data.qualisScoreValue == 'B4'] = 0.25
 data.qualisScoreValue[data.qualisScoreValue == 'B5'] = 0.10 
 data.qualisScoreValue[data.qualisScoreValue == 'C'] = 0.05 
 
-def groupSumSort(data, groupBy, sortBy=False, ascending=True):
-    df = data.groupby(by=groupBy).sum()
+def groupSumSort(data, groupBy, sortBy=False, ascending=True, top=False, func='sum'):
+    df = data.groupby(by=groupBy)
+    
+    if func=='sum':
+        df = df.sum().reset_index()
+    else:
+        if func=='mean':
+            df = df.mean().reset_index()
+
     df = df[:-1]
     if sortBy:
         df = df.sort_values(by=sortBy, ascending=ascending)
-    return df.reset_index()
+    if top:
+        df = df.groupby(top[0]).head(top[1])
+        df = df.sort_values(by=top[0]).reset_index()
+    return df
 
 # imprimindo o score geral por ano
 input = data.drop(["grupoName", "areaConhecimentoId","areaConhecimento"], axis=1)
 df = groupSumSort(input, ['ano'])
-graph.plot_line(
-    df['qualisScoreValue'], 
-    ylabel='Total Qualis Score', 
-    xlabel='Ano'
-)
+# graph.plot_line(
+#     df['qualisScoreValue'], 
+#     ylabel='Total Qualis Score', 
+#     xlabel='Ano'
+# )
 
 # imprimindo o score geral por grupo
 input = data.drop(["ano","areaConhecimentoId","areaConhecimento"], axis=1)
 df = groupSumSort(input, ['grupoName'], ['qualisScoreValue'], True)
-graph.plot_barh(
-    data=df['qualisScoreValue'], 
-    names=df['grupoName'], 
-    xlabel='Score',
-    title='Score geral por grupo'
-)
+# graph.plot_barh(
+#     data=df['qualisScoreValue'], 
+#     names=df['grupoName'], 
+#     xlabel='Score',
+#     title='Score geral por grupo'
+# )
 
 # imprimindo o score geral por area
 input = data.drop(["ano","grupoName","areaConhecimentoId"], axis=1)
 df = groupSumSort(input, ['areaConhecimento'], ['qualisScoreValue'], False)
-graph.plot_barh(
-    data=df['qualisScoreValue'], 
-    names=df['areaConhecimento'], 
-    xlabel='Score',
-    title='Score geral por area'
-)
+# graph.plot_barh(
+#     data=df['qualisScoreValue'], 
+#     names=df['areaConhecimento'], 
+#     xlabel='Score',
+#     title='Score geral por area'
+# )
+
+# imprimingo as 3 areas que mais publicaram
+input = data.drop(["grupoName","areaConhecimentoId"], axis=1)
+df = groupSumSort(input, ['ano', 'areaConhecimento'], ['ano', 'qualisScoreValue'], [True, False])
+gp = df.reset_index().head(3).groupby(['areaConhecimento'])
+for area in gp.groups:
+    plt.plot('ano', 'qualisScoreValue', data=df[df.areaConhecimento == area], label=area)
+
+plt.xlabel('Ano de publicação')
+plt.ylabel('Total score')
+plt.legend()
+plt.show()
---
+++ trabalho_4/clustering2.py
@@ -0,0 +1,77 @@
+"""
+"""
+import sys
+import os
+sys.path.append(os.path.abspath("../helpers"))
+
+import graph
+from matplotlib import pyplot as plt
+import numpy as np
+
+import pandas as pd
+data = pd.read_csv(
+    'Cientometria\\resultadoConsultaSJR.txt', 
+    header=0,
+    names = ["ano","sjrJournalId","areaConhecimento","sjrScoreValue","nomeGrupo","nomeAutor","nomePaper"],
+)
+
+data = data.drop('sjrJournalId',axis=1)
+data = data.drop('nomePaper',axis=1)
+
+def groupSumSort(data, groupBy, sortBy=False, ascending=True, top=False, func='sum'):
+    df = data.groupby(by=groupBy)
+    
+    if func=='sum':
+        df = df.sum().reset_index()
+    else:
+        if func=='mean':
+            df = df.mean().reset_index()
+
+    df = df[:-1]
+    if sortBy:
+        df = df.sort_values(by=sortBy, ascending=ascending)
+    if top:
+        df = df.groupby(top[0]).head(top[1])
+        df = df.sort_values(by=top[0]).reset_index()
+    return df
+
+# imprimindo o score geral por ano
+input = data.drop(["nomeGrupo","areaConhecimento"], axis=1)
+df = groupSumSort(input, ['ano'])
+graph.plot_line(
+    df['sjrScoreValue'], 
+    ylabel='Total Qualis Score', 
+    xlabel='Ano'
+)
+
+# imprimindo o score geral por grupo
+input = data.drop(["ano","areaConhecimento"], axis=1)
+df = groupSumSort(input, ['nomeGrupo'], ['sjrScoreValue'], True)
+graph.plot_barh(
+    data=df['sjrScoreValue'], 
+    names=df['nomeGrupo'], 
+    xlabel='Score',
+    title='Score geral por grupo'
+)
+
+# imprimindo o score geral por area
+input = data.drop(["ano","nomeGrupo"], axis=1)
+df = groupSumSort(input, ['areaConhecimento'], ['sjrScoreValue'], False)
+graph.plot_barh(
+    data=df['sjrScoreValue'], 
+    names=df['areaConhecimento'], 
+    xlabel='Score',
+    title='Score geral por area'
+)
+
+# imprimingo as 3 areas que mais publicaram
+input = data.drop(["nomeGrupo"], axis=1)
+df = groupSumSort(input, ['ano', 'areaConhecimento'], ['ano', 'sjrScoreValue'], [True, False])
+gp = df.reset_index().head(3).groupby(['areaConhecimento'])
+for area in gp.groups:
+    plt.plot('ano', 'sjrScoreValue', data=df[df.areaConhecimento == area], label=area)
+
+plt.xlabel('Ano de publicação')
+plt.ylabel('Total score')
+plt.legend()
+plt.show()
